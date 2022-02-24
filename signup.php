214c391acd4b0faea87a8b2a3dba452acc1bfa4e
AqeelSha7 <maqesha7@gmail.com>
2022-02-24 22:38:00 +0530

    added the signup file

--- signup.php
+++ signup.php
@@ -5,7 +5,58 @@ echo "<title>".$pagename."</title>"; //display name of the page as window title
 echo "<body>";
 include ("headfile.html"); //include header layout file 
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-//display random text
+
+
+echo "<form action=signup_process.php  method=post>";
+echo "<table style='border: 0px'>";
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>First Name:</td>";
+echo "<td style='border: 0px'><input type='text' name='fname'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Last Name:</td>";
+echo "<td style='border: 0px'><input type='text' name='lname'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Address:</td>";
+echo "<td style='border: 0px'><input type='text' name='address'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Postcode:</td>";
+echo "<td style='border: 0px'><input type='text' name='postcode'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Tel No:</td>";
+echo "<td style='border: 0px'><input type='text' name='tel'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Email Address:</td>";
+echo "<td style='border: 0px'><input type='text' name='email'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Password:</td>";
+echo "<td style='border: 0px'><input type='password' name='password'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'>Confirm Password:</td>";
+echo "<td style='border: 0px'><input type='password' name='passwordCon'>";
+echo "</tr>";
+
+echo "<tr style='border: 0px'>";
+echo "<td style='border: 0px'><input id='submitbtn' type='submit' value='Sign Up'></td>";
+echo "<td style='border: 0px'><input id='submitbtn' type='reset' value='Clear Form'></td>";
+echo "</tr>";
+
+echo "</table>";
+echo "</form>";
+
 include("footfile.html"); //include head layout
 echo "</body>";
 ?>
