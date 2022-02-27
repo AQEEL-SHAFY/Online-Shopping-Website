c9aae446f6563e70852944a246fb6c93d4d85daf
AqeelSha7 <maqesha7@gmail.com>
2022-02-28 00:37:39 +0530

    signup process file added and signup file has been updated

--- signup_process.php
+++ signup_process.php
@@ -7,17 +7,25 @@ echo "<title>".$pagename."</title>"; //display name of the page as window title
 echo "<body>";
 include ("headfile.html"); //include header layout file 
 echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
-//display random text
-echo "<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-labore et dolore magna aliqua. Non consectetur a erat nam at lectus urna. Cras pulvinar mattis nunc sed 
-blandit libero volutpat sed cras. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. 
-Nunc consequat interdum varius sit. Nam aliquam sem et tortor consequat. Magna sit amet purus gravida. Non 
-sodales neque sodales ut etiam sit. Tortor consequat id porta nibh venenatis. Ornare arcu odio ut sem 
-nulla pharetra diam. Tincidunt ornare massa eget egestas purus. Pulvinar mattis nunc sed blandit libero 
-volutpat sed. Nulla malesuada pellentesque elit eget. Varius quam quisque id diam vel quam elementum 
-pulvinar. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Vestibulum lectus mauris ultrices 
-eros in. Faucibus in ornare quam viverra. Hac habitasse platea dictumst vestibulum rhoncus. Parturient 
-montes nascetur ridiculus mus. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna.";
+
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$address=$_POST['address'];
+$postcode=$_POST['postcode'];
+$tel=$_POST['tel'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$passwordCon=$_POST['passwordCon'];
+
+$query="INSERT INTO Users (userType, userFName, userSName, userAddress, userPostcode, userTellNo, userEmail, userPassword) 
+VALUES ('C','$fname','$lname','$address','$postcode','$tel','$email','$password')";
+//Capture the 7 inputs entered in the 7 fields of the form using the $_POST superglobal variable
+//Store these details into a set of 7 new local variables
+//Write a SQL query to insert a new user into users table 
+//The SQL code for inserting a new record is 
+//INSERT INTO table_name (field1, field2, field3) VALUES (value1, value2, value3)
+//Execute the INSERT INTO SQL query
+mysqli_query($conn, $query);
 include("footfile.html"); //include head layout
 echo "</body>";
 ?>
