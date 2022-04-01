9a54d1f9432a68e42f90fdba1cd1bebe228c91e4
AqeelSha7 <maqesha7@gmail.com>
2022-04-02 01:40:23 +0530

    changes in login file

--- login_process.php
+++ login_process.php
@@ -26,43 +26,56 @@ if (empty($email) || empty($password)) {
     echo "<p>Make sure you provide all the reqired details<p>";
     echo "<p>Go back to <a href='login.php'>login</a></p>";
 } else {
-    $SQL = "select * from `users` where `userEmail`='$email'";
-	$result = mysqli_query($conn,$SQL);
-	$data = mysqli_fetch_array($result);
+    $SQL = "SELECT * FROM Users WHERE userEmail='".$email."'";
+	$exeSQL = mysqli_query($conn,$SQL) or die (mysqli_error($conn));
+    $nbrecs = mysqli_num_rows($exeSQL); 
+    $arrayu = mysqli_fetch_array($exeSQL); 
+   
     //SQL query to retrieve the record from the users table for which the email matches login email (in form)
     //execute the SQL query & fetch results of the execution of the SQL query and store them in $arrayu 
     //also capture the number of records retrieved by the SQL query using function mysqli_num_rows and store it 
     //in a variable called $nbrecs
     //if the number of records is 0 (i.e. email retrieved from the DB does not match $email login email in form
-}
-if ($data['userEmail'] != $email) {
-    //display error message "Email not recognised, login again"
-    echo "<p>Login Failed!</p>";
-    echo "<p>Email not recognised</p>";
-    echo "<p>Go back to <a href='login.php'>login</a></p>";
-} else {
-    //if password retrieved from the database (in arrayu) does not match $password    
-    if ($data['userEmail'] != $email) {
-        //display error message "Password not recognised, login again"
+    if ($nbrecs == 0) {
+        //display error message "Email not recognised, login again"
         echo "<p>Login Failed!</p>";
-        echo "<p>Password you entered was not recognised</p>";
+        echo "<p>Email not recognised</p>";
         echo "<p>Go back to <a href='login.php'>login</a></p>";
-    } else {
-        //display login success message and store user id, user type, name into 4 session variables i.e.
-        //create $_SESSION['userid'], $_SESSION['usertype'], $_SESSION['fname'], $_SESSION['sname'],
-        $_SESSION['userid'] = $data['userId'];
-        $_SESSION['usertype'] = $data['userType'];
-        $_SESSION['fname'] = $data['userFName'];
-        $_SESSION['sname'] = $data['userSName'];
-        //Greet the user by displaying their name using $_SESSION['fname'] and $_SESSION['sname']
-        echo "<p><b>Login Success</b></p>";
-        echo "<p> Hello, " . $data['userFName'] . " " . $data['userSName'] . "<p>";
-        //Welcome them as a customer by using $_SESSION['usertype '] 
-        if ($data['userType'] == 'C') {
-            echo "<p> You have successfully logged in as a homteq Customer<p>";
-            echo "<p>Continue to shopping for <a href='index.php'>Home Tech</a></p>";
-            echo "<p>Go back to <a href='basket.php'>Shopping Cart Page</a></p>";
-        }
+    }else
+{
+  
+    //if password retrieved from the database (in arrayu) does not match $password
+    if(!$arrayu['userPassword'] = $password)
+    {
+    //display error message "Password not recognised, login again"
+    echo "<p><b>Login failed!</b>"; //display login error
+    echo "<br>Password not valid</p>";
+    echo "<br><p> Go back to <a href=login.php>login</a></p>";
+    }
+    else
+    {
+    //display login success message and store user id, user type, name into 4 session variables i.e.
+    echo "<p><b>Login success</b></p>"; 
+    $_SESSION['userid'] = $arrayu['userId']; 
+    $_SESSION['fname'] = $arrayu['userFName']; 
+    $_SESSION['sname'] = $arrayu['userSName']; 
+    $_SESSION['usertype'] = $arrayu['userType'];
+    //create $_SESSION['userid'], $_SESSION['usertype'], $_SESSION['fname'], $_SESSION['sname'],
+    echo "<p> Welcome, ".$_SESSION['fname']." ".$_SESSION['sname']."</p>";//Greet the user by displaying their name using $_SESSION['fname'] and $_SESSION['sname']
+    if ($_SESSION['usertype']=='C') //if user type is C, they are a customer
+    {
+    echo "<p>User Type: homteq Customer</p>";
+    }
+    if ($_SESSION['usertype']=='A') //if user type is A, they are an admin
+    {
+    echo "<p>User type: homteq Administrator</p>";
+    }
+
+    echo "<br><p>Continue shopping for <a href=index.php>Home Tech</a>";
+    echo "<br>View your <a href=basket.php>Smart Basket</a></p>";
+
+    //Welcome them as a customer by using $_SESSION['usertype '] 
+    }
     }
 }
 
@@ -70,3 +83,31 @@ if ($data['userEmail'] != $email) {
 
 include("footfile.html"); //include head layout
 echo "</body>";
+
+ // else {
+//         //if password retrieved from the database (in arrayu) does not match $password    
+//         if ($data['userEmail'] != $email) {
+//             //display error message "Password not recognised, login again"
+//             echo "<p>Login Failed!</p>";
+//             echo "<p>Password you entered was not recognised</p>";
+//             echo "<p>Go back to <a href='login.php'>login</a></p>";
+//         }
+// }
+//   else {
+//         //display login success message and store user id, user type, name into 4 session variables i.e.
+//         //create $_SESSION['userid'], $_SESSION['usertype'], $_SESSION['fname'], $_SESSION['sname'],
+//         $_SESSION['userid'] = $data['userId'];
+//         $_SESSION['usertype'] = $data['userType'];
+//         $_SESSION['fname'] = $data['userFName'];
+//         $_SESSION['sname'] = $data['userSName'];
+//         //Greet the user by displaying their name using $_SESSION['fname'] and $_SESSION['sname']
+//         echo "<p><b>Login Success</b></p>";
+//         echo "<p> Hello, " . $data['userFName'] . " " . $data['userSName'] . "<p>";
+//         //Welcome them as a customer by using $_SESSION['usertype '] 
+//         if ($data['userType'] == 'C') {
+//             echo "<p> You have successfully logged in as a homteq Customer<p>";
+//             echo "<p>Continue to shopping for <a href='index.php'>Home Tech</a></p>";
+//             echo "<p>Go back to <a href='basket.php'>Shopping Cart Page</a></p>";
+//         }
+//     }
+// }
